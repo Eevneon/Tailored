4f8be776891065e05a83cf040745d7404e0c5ac3
Eevneon <bartpeters09@gmail.com>
2024-12-09 21:38:13 +0100

    Initial commit

---
+++ src/main/java/eevneon/tailored/Tailored.java
@@ -0,0 +1,14 @@
+package eevneon.tailored;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Tailored implements ModInitializer {
+	public static final String MOD_ID = "tailored";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {}
+}
---
+++ src/main/java/eevneon/tailored/TailoredClient.java
@@ -0,0 +1,8 @@
+package eevneon.tailored;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TailoredClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {}
+}
